f3abd6c438e43f90c19320d6732e8438c5c7ab8c
gs-ubuntu <841188395@qq.com>
2023-08-19 18:14:23 +0800

    整理用户模块代码

--- service/user_service_test.go
+++ service/user_service_test.go
@@ -1,35 +1,40 @@
 package service
 
 import (
-    "encoding/json"
-    "testing"
+	"encoding/json"
+	"testing"
 
-    "simple_douyin/model"
+	"simple_douyin/middleware/database"
+	"simple_douyin/model"
 )
 
 func TestLogin(t *testing.T) {
-    req := model.LoginRequest{
-        UserName: "guest",
-        Password: "guest",
-    }
-    res := Login(req)
-    // 将结构体转为json字符串
-    resJson, _ := json.Marshal(res)
-    t.Log(string(resJson))
+	database.Init()
+	req := model.LoginRequest{
+		UserName: "guest",
+		Password: "guest",
+	}
+	service := NewUserServiceInstance()
+	res := service.Login(req)
+	// 将结构体转为json字符串
+	resJson, _ := json.Marshal(res)
+	t.Log(string(resJson))
 }
 
 func TestRegister(t *testing.T) {
-    req := model.RegisterRequest{
-        UserName: "guest",
-        Password: "guest",
-    }
-    res := Register(req)
-    resJson, _ := json.Marshal(res)
-    t.Log(string(resJson))
+	database.Init()
+	req := model.RegisterRequest{
+		UserName: "guest",
+		Password: "guest",
+	}
+	service := NewUserServiceInstance()
+	res := service.Register(req)
+	resJson, _ := json.Marshal(res)
+	t.Log(string(resJson))
 }
 
 func TestUserInfo(t *testing.T) {
-    res := UserInfo(7)
-    resJson, _ := json.Marshal(res)
-    t.Log(string(resJson))
+	res := userService.UserInfo(7)
+	resJson, _ := json.Marshal(res)
+	t.Log(string(resJson))
 }
