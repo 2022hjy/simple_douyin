46b75da7a71f759b4cf0cd0051c03e25c4b0703d
kixuan <1752845542@qq.com>
2023-08-23 02:53:22 +0800

    feat:更新sql的message和video

--- douyin.sql
+++ douyin.sql
@@ -5,8 +5,7 @@ create database douyin;
 use douyin;
 -- Table structure for comments
 DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment`
-(
+CREATE TABLE `comment`  (
     `id`           bigint unsigned NOT NULL AUTO_INCREMENT UNIQUE COMMENT '评论的唯一ID',
     `user_info_id` bigint unsigned DEFAULT 0 COMMENT '发表评论的用户的引用ID',
     `video_id`     bigint unsigned DEFAULT 0 COMMENT '被评论的视频的引用ID',
@@ -15,7 +14,7 @@ CREATE TABLE `comment`
     PRIMARY KEY (`id`),
     KEY            `fk_videos_comments` (`video_id`) USING BTREE,
     KEY            `fk_user_infos_comments` (`user_info_id`) USING BTREE,
-    FULLTEXT INDEX `idx_content` (`content`),
+  FULLTEXT INDEX `idx_content`(`content`),
     CONSTRAINT `fk_user_infos_comments` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`id`),
     CONSTRAINT `fk_videos_comments` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='评论表';
@@ -37,7 +36,53 @@ CREATE TABLE `favorite`
     CONSTRAINT `fk_user_favorite_video` FOREIGN KEY (`video_id`) REFERENCES `video` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='用户点赞视频表';
 
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '聊天id',
+  `from_user_id` bigint UNSIGNED NOT NULL COMMENT '发送者id',
+  `to_user_id` bigint UNSIGNED NOT NULL COMMENT '接收者id',
+  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '消息内容',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发送时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `to_user_id`(`to_user_id` ASC) USING BTREE,
+  INDEX `from_user_id`(`from_user_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '聊天消息' ROW_FORMAT = DYNAMIC;
 
+-- ----------------------------
+-- Records of message
+-- ----------------------------
+INSERT INTO `message` VALUES (1, 1, 2, '测试消息', '2023-08-22 03:49:40');
+INSERT INTO `message` VALUES (2, 1, 2, '测试消息2', '2023-08-22 13:17:10');
+INSERT INTO `message` VALUES (3, 10, 2, '2 号用户发送消息给 8 号用户', '2023-08-22 13:31:14');
+INSERT INTO `message` VALUES (6, 10, 2, '2 - 10', '2023-08-22 13:45:43');
+INSERT INTO `message` VALUES (7, 2, 26, '226', '2023-08-22 13:50:39');
+INSERT INTO `message` VALUES (9, 2, 26, '2-26', '2023-08-22 13:52:05');
+INSERT INTO `message` VALUES (10, 2, 26, '226', '2023-08-22 14:01:05');
+
+-- ----------------------------
+-- Table structure for relation
+-- ----------------------------
+DROP TABLE IF EXISTS `relation`;
+CREATE TABLE `relation`  (
+  `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '关系ID',
+  `user_id` bigint UNSIGNED NOT NULL COMMENT '用户ID',
+  `following_id` bigint UNSIGNED NOT NULL COMMENT '关注的用户ID',
+  `is_followed` tinyint UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否已关注，1表示已关注，0表示未关注',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_relation_user_id`(`user_id` ASC) USING BTREE,
+  INDEX `idx_relation_following_id`(`following_id` ASC) USING BTREE,
+  CONSTRAINT `fk_relation_following` FOREIGN KEY (`following_id`) REFERENCES `user_info` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `fk_relation_user` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户关系表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `user_id` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '登录记录的唯一ID',
@@ -50,28 +95,11 @@ CREATE TABLE `user`  (
   UNIQUE INDEX `idx_username`(`username` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户信息表' ROW_FORMAT = DYNAMIC;
 
-
--- Table structure for relation
-DROP TABLE IF EXISTS `relation`;
-CREATE TABLE `relation`
-(
-    `id`           bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '关系ID',
-    `user_id`      bigint unsigned NOT NULL COMMENT '用户ID',
-    `following_id` bigint unsigned NOT NULL COMMENT '关注的用户ID',
-    `is_followed`  tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '是否已关注，1表示已关注，0表示未关注',
-    `created_at`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '记录创建时间',
-    `updated_at`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '记录更新时间',
-    PRIMARY KEY (`id`) USING BTREE,
-    key            `idx_relation_user_id` (`user_id`) USING BTREE,
-    key            `idx_relation_following_id` (`following_id`) USING BTREE,
-    CONSTRAINT `fk_relation_user` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-    CONSTRAINT `fk_relation_following` FOREIGN KEY (`following_id`) REFERENCES `user_info` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='用户关系表';
-
+-- ----------------------------
 -- Table structure for video
+-- ----------------------------
 DROP TABLE IF EXISTS `video`;
-CREATE TABLE `video`
-(
+CREATE TABLE `video`  (
     `id`             bigint unsigned NOT NULL AUTO_INCREMENT  unique COMMENT '视频的唯一ID',
     `user_info_id`   bigint unsigned DEFAULT 0 COMMENT '上传视频的用户的引用ID',
     `play_url`       varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '视频播放URL',
@@ -84,19 +112,8 @@ CREATE TABLE `video`
     `updated_at`     datetime(3) DEFAULT NULL COMMENT '视频上次更新时间',
     PRIMARY KEY (`id`),
     KEY              `fk_user_info_video` (`user_info_id`),
-    FULLTEXT INDEX `idx_title` (`title`),
-    CONSTRAINT `fk_user_info_video` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='视频信息表';
+  FULLTEXT INDEX `idx_title`(`title`),
+  CONSTRAINT `fk_user_info_video` FOREIGN KEY (`user_info_id`) REFERENCES `user_info` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '视频信息表' ROW_FORMAT = DYNAMIC;
 
-DROP TABLE IF EXISTS `user_chat`;
-CREATE TABLE `user_chat`
-(
-    msg_id      BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '聊天id',
-    sender_id    BIGINT UNSIGNED NOT NULL COMMENT '发送者用户id',
-    recipient_id BIGINT UNSIGNED NOT NULL COMMENT '接收者用户id',
-    msg_content      TEXT      NOT NULL COMMENT '消息内容',
-    send_time    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '发送时间',
-    PRIMARY KEY (msg_id),
-    FOREIGN KEY (sender_id) REFERENCES `user_info` (`id`),
-    FOREIGN KEY (recipient_id) REFERENCES `user_info` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='存储私人聊天记录' ROW_FORMAT=DYNAMIC;
+SET FOREIGN_KEY_CHECKS = 1;
